e648b941e7bba53a498bbb683dab1c371421aad7
nagred01 <45577214+nagred01@users.noreply.github.com>
2019-01-17 19:13:16 -0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -80,7 +80,86 @@
                                     style : styles.loginButtonLabel,
                                 }, ["Login"])]
                             )
-
+				  loginCall() {
+        let userJsonData = { "loginName": this.state.userName, "password": this.state.password }
+        fetch('https://cfsfiserv.com/QEUATSMT/api/Authentication/LogIn',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userJsonData),
+            }).then(response => {
+                this.setState({progressModal:false});
+                var responseObj = JSON.parse(response._bodyText);
+                var TokenResponse = responseObj.antiForgeryToken;
+                //console.log("responseObj  =::" + responseObj.antiForgeryToken);
+                if (TokenResponse == '' || TokenResponse == undefined) {
+                    this.setState({progressModal:false});
+                    Alert.alert(
+                        '',
+                        'Please enter the valid UserName and Password',
+                        [
+                            { text: '', onPress: () => console.log('Ask me later pressed') },
+                            { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+                            { text: 'OK', onPress: () => console.log('OK Pressed') },
+                        ],
+                        { cancelable: false }
+                    )
+                } else {
+                    this.setState({progressModal:false});
+                    this.props.navigation.navigate("AccountSummary", {
+                        token: TokenResponse,
+                    });
+                }
+            });
+    }
+    validateUser() {
+        if (this.state.userName === '' || this.state.userName == undefined) {
+            Toast.show({
+                text: 'Please enter Username',
+                position: 'bottom',
+                buttonText: 'Okay',
+                duration: 5000,
+                type: 'danger',
+            })
+        }
+        else if (this.state.password === '' || this.state.password == undefined) {
+            Toast.show({
+                text: 'Please enter Password',
+                position: 'bottom',
+                buttonText: 'Okay',
+                duration: 5000,
+                type: 'danger',
+            })
+        }
+        /*else if (!this.isValidPassword(this.state.password) || this.state.password.length <= 8) {
+            Toast.show({
+                text: "Password should have minimum 8 characters with 1 numeric and 1 alphabet",
+                position: 'bottom',
+                buttonText: 'Okay',
+                duration: 5000,
+                type: 'danger',
+            })
+        } */
+        else {
+            this.setState({progressModal:true});
+            return(<Overlay
+                visible={this.state.progressModal}
+                        //closeOnTouchOutside
+                         animationType="zoomIn"
+                         containerStyle={{backgroundColor: 'rgba(37, 8, 10, 0.78)'}}
+                         childrenWrapperStyle={{backgroundColor: '#eee'}}
+                         animationDuration={100}>
+                         <View>
+                             <ActivityIndicator size="large" color="#3491cc"/>
+                            <Text>Please wait a moment...</Text>
+                        </View>
+                   </Overlay>,this.loginCall());
+            //this.setState({requestLoading:true});
+            //this.loginCall();
+        }
+    }
                     ])
             ])
         
