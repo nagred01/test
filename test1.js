5890ad96d1adf944a3150eab27d26bd2fa4d2469
nagred01 <45577214+nagred01@users.noreply.github.com>
2019-01-17 19:09:42 -0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,1 +1,88 @@
+(function main(React, ReactNative,NativeBase,componentState, styles, require) {
+    'use strict';
 
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+    var react_1 = React;
+    var _reactNative = ReactNative;
+    var _nativebase = NativeBase;
+    var root = this;
+    
+
+    return react_1.createElement(_nativebase.Container, {style:styles.containerStyle }, [
+                react_1.createElement(_reactNative.View, {
+                        "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04_field_container",
+                        style:styles.viewStyle
+                    }, [
+                            react_1.createElement(_reactNative.Image, {
+                                    "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl0423_Label_container",
+                                    style:styles.imageStyle,
+                                    source:{uri:'https://raw.githubusercontent.com/nagred01/Login/master/Mainlog.png'}
+                            }, null),
+                            react_1.createElement(_reactNative.View, {
+                                    "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04_Label_container",
+                                    style:styles.userNameItem
+                                }, [
+                                        react_1.createElement(_nativebase.Label, {
+                                        "htmlFor": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04",
+                                        "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04_Label",
+                                        style : styles.userNameLabel
+                                        },["UserName"]),
+                                        react_1.createElement(_reactNative.TextInput, {
+                                            "id": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04",
+                                            "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04",
+                                            "cssClass": "form-control component-group",
+                                            "fieldCssClass": "",
+                                            style : styles.inputStyle,
+                                            autoCapitalize : 'none',
+                                            "bindingMode": "",
+                                            onChangeText: function (val) {
+                                                componentState.setState({ userName: val })
+                                            },
+                                            placeHolder: "Enter the User Name"
+                                        }, [])
+                                ]),
+                                react_1.createElement(_reactNative.View, {
+                                    "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04_Label_container",
+                                    style:styles.userNameItem
+                                }, [
+                                    react_1.createElement(_nativebase.Label, {
+                                        "htmlFor": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04",
+                                        "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04_Label",
+                                        style : styles.passwordlabel
+                                    },["Password"]),
+                                    react_1.createElement(_reactNative.TextInput, {
+                                        "id": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04",
+                                        "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl04",
+                                        "cssClass": "form-control component-group",
+                                        "fieldCssClass": "",
+                                        style : styles.inputStyle,
+                                        "bindingMode": "",
+                                        onChangeText: function (val) {
+                                            componentState.setState({ password: val })
+                                        },
+                                         autoCapitalize : 'none',
+                                        secureTextEntry:true,
+                                        placeHolder: "Enter the Password"
+                                    }, [])
+                                ]),
+                            react_1.createElement(_nativebase.Button, {
+                                    "id": "M_layout_content_PCDZ_MNS7LAN_ctl00_btnCancel",
+                                    "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_btnCancel",
+                                    style : styles.loginButton,
+				   onPress: function(){
+                                           componentState.validateUser()
+                                    }
+                                },[react_1.createElement(_reactNative.Text, {
+                                    "htmlFor": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl023",
+                                    "key": "M_layout_content_PCDZ_MNS7LAN_ctl00_ctl023_Label",
+                                    style : styles.loginButtonLabel,
+                                }, ["Login"])]
+                            )
+
+                    ])
+            ])
+        
+    
+})
